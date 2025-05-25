ba3b07b317e7da6e78592adb6aa42628e3299cf3
aldenzho <162027669+aldenzho@users.noreply.github.com>
2025-05-25 18:36:03 +0700

    Add files via upload

---
+++ file_protocol.py
@@ -0,0 +1,48 @@
+import json
+import logging
+import shlex
+
+from file_interface import FileInterface
+
+"""
+* class FileProtocol bertugas untuk memproses 
+data yang masuk, dan menerjemahkannya apakah sesuai dengan
+protokol/aturan yang dibuat
+
+* data yang masuk dari client adalah dalam bentuk bytes yang 
+pada akhirnya akan diproses dalam bentuk string
+
+* class FileProtocol akan memproses data yang masuk dalam bentuk
+string
+"""
+
+
+
+
+class FileProtocol:
+    def _init_(self):
+        self.file = FileInterface()
+    def proses_string(self, string_datamasuk=''):
+        logging.warning(f"string diproses: {string_datamasuk}")
+        try:
+            # Jangan lowercase seluruh string karena base64 case sensitive
+            # Pecah dengan shlex, yang bisa menangani quotes dll
+            c = shlex.split(string_datamasuk)
+            if len(c) == 0:
+                return json.dumps(dict(status='ERROR', data='request kosong'))
+            c_request = c[0].lower()  # command hanya lowercase saja
+            logging.warning(f"memproses request: {c_request}")
+            params = c[1:]
+            cl = getattr(self.file, c_request)(params)
+            return json.dumps(cl)
+        except AttributeError:
+            return json.dumps(dict(status='ERROR', data='request tidak dikenali'))
+        except Exception as e:
+            logging.error(f"error processing request: {str(e)}")
+            return json.dumps(dict(status='ERROR', data=str(e)))
+
+if _name=='main_':
+    #contoh pemakaian
+    fp = FileProtocol()
+    print(fp.proses_string("LIST"))
+    print(fp.proses_string("GET pokijan.jpg"))
